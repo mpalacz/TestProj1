ec3a084aaa35022321d0a2999a50b2433a9de360
mpala <mpalacz00@gmail.com>
2023-02-24 15:23:57 +0100

    Subtract test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -3,14 +3,21 @@ namespace TestProj1
     [TestClass]
     public class UnitTest1
     {
+        double a = 1.454458;
+        double b = -84.32;
         [TestMethod]
         public void TestMethod_add()
         {
-            double a = 1.5;
-            double b = 2.25;
-            double expected = 3.75;
+            double expected = -82.86542;
             double actual = MathOperations.addition(a, b);
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, actual, 0.001);
+        }
+        [TestMethod]
+        public void TestMethod_subtract()
+        {
+            double expected = 85.774458;
+            double actual = MathOperations.subtractition(a, b);
+            Assert.AreEqual(expected, actual, 0.001);
         }
     }
 }
