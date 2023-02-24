96bbc7839e21defcea220f087b76489c009b9aca
mpala <mpalacz00@gmail.com>
2023-02-24 15:07:45 +0100

    MathOperations

---
+++ MathOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProj1
+{
+    internal class MathOperations
+    {
+        public static double addition(double a, double b)
+        {
+            return a + b;
+        }
+        public static double subtractition(double a, double b)
+        {
+            return a - b;
+        }
+        public static double multiplication(double a, double b)
+        {
+            return a * b;
+        }
+        public static double division(double a, double b)
+        {
+            if (a == 0 || b == 0)
+                return 0;
+            return a / b;
+        }
+    }
+}
