b95e3ba3a326ee8e9f2ebfe9f68b56866e2e9063
mpala <mpalacz00@gmail.com>
2023-02-24 15:37:09 +0100

    Multiplication and division tests

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,19 +5,36 @@ namespace TestProj1
     {
         double a = 1.454458;
         double b = -84.32;
+        double expected, actual;
         [TestMethod]
         public void TestMethod_add()
         {
-            double expected = -82.86542;
-            double actual = MathOperations.addition(a, b);
+            expected = -82.86542;
+            actual = MathOperations.addition(a, b);
             Assert.AreEqual(expected, actual, 0.001);
         }
         [TestMethod]
-        public void TestMethod_subtract()
+        public void TestMethod_sub()
         {
-            double expected = 85.774458;
-            double actual = MathOperations.subtractition(a, b);
+            expected = 85.774458;
+            actual = MathOperations.subtractition(a, b);
             Assert.AreEqual(expected, actual, 0.001);
+        }
+        [TestMethod]
+        public void TestMethod_mul()
+        {
+            expected = -122.63989856;
+            actual = MathOperations.multiplication(a, b);
+            Assert.AreEqual(expected, actual, 0.001);
+        }
+        [TestMethod]
+        public void TestMethod_div()
+        {
+            expected = -0.0172492647058824;
+            actual = MathOperations.division(a, b);
+            Assert.AreEqual(expected, actual, 0.001);
+            actual = MathOperations.division(a, 0);
+            Assert.AreEqual(0, actual);
         }
     }
 }
